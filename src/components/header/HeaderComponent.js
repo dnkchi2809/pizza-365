ed2d9b25718ab2e8c92bec311b152db683204392
chidnk <chidnk@devcamp.edu.vn>
2022-07-18 22:57:45 +0700

    commit

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -13,7 +13,7 @@ function HeaderComponent() {
                                     <a className="nav-link" href="/">Trang chủ<span className="sr-only">(current)</span></a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/" href="#pizza-combo">Combo</a>
+                                    <a className="nav-link" href="/#pizza-combo">Combo</a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="/#pizza-type">Loại Pizza</a>
